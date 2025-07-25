fe7b891cae23e8cf7e4f27d526c32ea9661a1a62
Aaryan meena <134821046+aaryan359@users.noreply.github.com>
2025-07-26 01:48:25 +0530

    Update Addsubtopic.jsx

--- frontend/src/pages/nontechadminparts/Addsubtopic.jsx
+++ frontend/src/pages/nontechadminparts/Addsubtopic.jsx
@@ -77,7 +77,7 @@ const Addsubtopicnontech = () => {
                const loadingToast = toast.loading('Uploading videos & QA...');
       try {
         // Send API request to add subtopic contributions
-        const response = await axios.post('https://opencoursebackend.onrender.com/nontech/addNonTechSubtopic', payload, {
+        const response = await axios.post('https://opencoursebackend-j3sa.onrender.com/nontech/addNonTechSubtopic', payload, {
           headers: {
             Authorization: `Bearer ${token}`, // Include authentication token
           },
